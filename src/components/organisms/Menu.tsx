815f92d60c35b22e4a920ddec2a96be426af2f2f
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-12 17:33:39 -0400

    feat(component): ✨ create component Menu

---
+++ src/components/organisms/Menu.tsx
@@ -0,0 +1,13 @@
+export const Menu = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Login with Google</li>
+          <li>Login with Github</li>
+          <li>Login with Discord</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
