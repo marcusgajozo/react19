53e20d098aac2bd36751026cb77ec68974079827
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-12 17:36:46 -0400

    feat(app): ✨ wrap page with Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Layout } from "./components/templates/Layout";
+
 function App() {
   return (
-    <>
+    <Layout>
       <h1 className="text-2xl font-bold underline">Hello, world!</h1>
-    </>
+    </Layout>
   );
 }
 
