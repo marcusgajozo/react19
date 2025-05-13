2bb3eda39e5fa7489a92b96dd0a3534121be7a5b
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-12 20:59:44 -0400

    feat(route): ✨add new route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "../components/templates/Layout";
 import { Home } from "../pages/Home/Home";
+import { LoginGoogle } from "../pages/LoginGoogle/LoginGoogle";
 
 export const routes = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const routes = createBrowserRouter([
       {
         path: "/",
         Component: Home,
+      },
+      {
+        path: "/login-google",
+        Component: LoginGoogle,
       },
     ],
   },
