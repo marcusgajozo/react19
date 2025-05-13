c73e2a78acd20bcf92c1ecf70302e007240f933a
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-12 21:00:35 -0400

    feat(menu): ✨add NavLink

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -1,12 +1,14 @@
+import { NavLink } from "react-router";
+
 export const Menu = () => {
   return (
     <aside className="w-xs bg-gray-50">
       <nav>
-        <ul>
-          <li>Login with Google</li>
-          <li>Login with Github</li>
-          <li>Login with Discord</li>
-        </ul>
+        <NavLink to="/login-google" end>
+          Login with Google
+        </NavLink>
+        <li>Login with Github</li>
+        <li>Login with Discord</li>
       </nav>
     </aside>
   );
