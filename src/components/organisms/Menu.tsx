937dc580f78acf0d90f3212e076f7c712e4eaaa4
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-12 23:42:16 -0400

    refactor(menu): organizes component

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -3,12 +3,16 @@ import { NavLink } from "react-router";
 export const Menu = () => {
   return (
     <aside className="w-xs bg-gray-50">
-      <nav>
+      <nav className="flex flex-col gap-2 p-4">
+        <NavLink to="/" end>
+          Home
+        </NavLink>
         <NavLink to="/login-google" end>
           Login with Google
         </NavLink>
-        <li>Login with Github</li>
-        <li>Login with Discord</li>
+        <NavLink to="/config-husky-commitlint" end>
+          Config Husky Commitlint
+        </NavLink>
       </nav>
     </aside>
   );
