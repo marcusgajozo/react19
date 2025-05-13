718c50fd00523de1b331cf0ca1bbc6a2632b1351
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-13 09:17:51 -0400

    refactor(menu): add menu title and linkedin link

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -2,8 +2,22 @@ import { NavLink } from "react-router";
 
 export const Menu = () => {
   return (
-    <aside className="w-xs bg-gray-50">
-      <nav className="flex flex-col gap-2 p-4">
+    <aside className="w-xs pl-4 pt-2 bg-gray-50">
+      <div className="">
+        <h3 className="text-2xl font-bold">Study React19</h3>
+        <p className="text-sm text-gray-500">
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/marcusgajozo/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-700 font-semibold"
+          >
+            Marcus Gajozo
+          </a>
+        </p>
+      </div>
+      <nav className="flex flex-col gap-2 mt-4">
         <NavLink to="/" end>
           Home
         </NavLink>
